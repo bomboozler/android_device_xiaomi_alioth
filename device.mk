33ebaa2eb2283b1904c78e6e8e9de9cb4eb1a088
bomboozler <yarinqwq121@gmail.com>
2025-12-14 16:19:22 +0200

    GameBar integration

--- device.mk
+++ device.mk
@@ -60,3 +60,6 @@ $(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
 # Remove Packages
 PRODUCT_PACKAGES += \
     RemovePackages
+
+# GameBar Performance Overlay
+$(call inherit-product, packages/apps/GameBar/gamebar.mk)
